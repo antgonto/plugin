8d6a1c8f41aa9d51ef73e1cf14a739171017ddb9
Gerald Valverde <gerald-_16@hotmail.com>
2020-11-03 17:38:24 -0600

    Socket Client Working

--- Extension/Socket/SocketCLient/SocketCLient/Program.cs
+++ Extension/Socket/SocketCLient/SocketCLient/Program.cs
@@ -1,66 +1,139 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 using SocketIOClient;
-
 namespace SocketCLient
 {
 
     // REFERENCIA https://archive.codeplex.com/?p=socketio4net
-    //https://www.codeproject.com/Articles/598281/SocketIO-Programming-in-Csharp-using-SocketIO4Net
+    // https://www.codeproject.com/Articles/598281/SocketIO-Programming-in-Csharp-using-SocketIO4Net
+
     class Program
     {
-        static Client socket;
-        public static String ip;
-        public static String port;
-
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Hello World!");
-            try
+            Console.WriteLine(Environment.OSVersion);
+            Console.OutputEncoding = Encoding.UTF8;
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+
+            var uri = new Uri("http://localhost:1617");
+
+            var socket = new SocketIO(uri, new SocketIOOptions
             {
-                socket = new Client("http://localhost:1617");
-                socket.On("txt", (data) =>
+                Query = new Dictionary<string, string>
                 {
-                    //MessageBox.Show(data.RawMessage);
-                    String msg = data.Json.Args[0].ToString();
-                    Console.Write(msg);
-                    //MessageBox.Show(msg, "Received Data");
-                });
-                socket.Connect();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Something Went Wrong!!");
-            }
-            if (socket.ReadyState.ToString() == "Connecting")
-            {
-                userset();
-            }
-            else
-            {
-                Console.WriteLine(socket.IsConnected.ToString());
-                Console.WriteLine(socket.ReadyState.ToString(), "Error!");
-                Console.WriteLine("Failed To Connect To Server!", "Error!");
-            }
-            Console.WriteLine("=============================");
-            Console.WriteLine("");
-            Console.ResetColor();
-        }
-        public static void emit(String msg)
-        {
-            socket.Emit("private message", port + " : " + msg);
+                    {"token", "io" }
+                },
+                EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls
+            });
 
+            socket.OnConnected += Socket_OnConnected;
+            socket.OnPing += Socket_OnPing;
+            socket.OnPong += Socket_OnPong;
+            socket.OnDisconnected += Socket_OnDisconnected;
+            socket.OnReconnecting += Socket_OnReconnecting;
+            await socket.ConnectAsync();
+
+            socket.On("hi", response =>
+            {
+                Console.WriteLine($"server: {response.GetValue<string>()}");
+            });
+
+            socket.On("bytes", response =>
+            {
+                var bytes = response.GetValue<ByteResponse>();
+                Console.WriteLine($"bytes.Source = {bytes.Source}");
+                Console.WriteLine($"bytes.ClientSource = {bytes.ClientSource}");
+                Console.WriteLine($"bytes.Buffer.Length = {bytes.Buffer.Length}");
+                Console.WriteLine($"bytes.Buffer.ToString() = {Encoding.UTF8.GetString(bytes.Buffer)}");
+            });
+            socket.OnReceivedEvent += (sender, e) =>
+            {
+                if (e.Event == "bytes")
+                {
+                    var bytes = e.Response.GetValue<ByteResponse>();
+                    Console.WriteLine($"OnReceivedEvent.Source = {bytes.Source}");
+                    Console.WriteLine($"OnReceivedEvent.ClientSource = {bytes.ClientSource}");
+                    Console.WriteLine($"OnReceivedEvent.Buffer.Length = {bytes.Buffer.Length}");
+                    Console.WriteLine($"OnReceivedEvent.Buffer.ToString() = {Encoding.UTF8.GetString(bytes.Buffer)}");
+                }
+            };
+
+            Console.ReadLine();
         }
-        public static void userset()
+
+        private static void Socket_OnReconnecting(object sender, int e)
         {
-            if (socket != null)
-                socket.Emit("newuser", port);
+            Console.WriteLine($"Reconnecting: attempt = {e}");
         }
-        public static void disco()
+
+        private static void Socket_OnDisconnected(object sender, string e)
         {
-            if (socket != null)
-                socket.Emit("exit", port);
-            socket.Close();
+            Console.WriteLine("disconnect: " + e);
         }
+
+        private static async void Socket_OnConnected(object sender, EventArgs e)
+        {
+            Console.WriteLine("Socket_OnConnected");
+            var socket = sender as SocketIO;
+            Console.WriteLine("Socket.Id:" + socket.Id);
+            await socket.EmitAsync("hi", "SocketIOClient.Sample");
+
+            //await socket.EmitAsync("ack", response =>
+            //{
+            //    Console.WriteLine(response.ToString());
+            //}, "SocketIOClient.Sample");
+
+            //await socket.EmitAsync("bytes", "c#", new
+            //{
+            //    source = "client007",
+            //    bytes = Encoding.UTF8.GetBytes("dot net")
+            //});
+
+            //socket.On("client binary callback", async response =>
+            //{
+            //    await response.CallbackAsync();
+            //});
+
+            //await socket.EmitAsync("client binary callback", Encoding.UTF8.GetBytes("SocketIOClient.Sample"));
+
+            //socket.On("client message callback", async response =>
+            //{
+            //    await response.CallbackAsync(Encoding.UTF8.GetBytes("CallbackAsync();"));
+            //});
+            //await socket.EmitAsync("client message callback", "SocketIOClient.Sample");
+        }
+
+        private static void Socket_OnPing(object sender, EventArgs e)
+        {
+            Console.WriteLine("Ping");
+        }
+
+        private static void Socket_OnPong(object sender, TimeSpan e)
+        {
+            Console.WriteLine("Pong: " + e.TotalMilliseconds);
+        }
+    }
+
+    class ByteResponse
+    {
+        public string ClientSource { get; set; }
+
+        public string Source { get; set; }
+
+        [JsonProperty("bytes")]
+        public byte[] Buffer { get; set; }
+    }
+
+    class ClientCallbackResponse
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("bytes")]
+        public byte[] Bytes { get; set; }
     }
 }
