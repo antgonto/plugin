52c22a20f6a104c8d18947ffdbb01e86ec7417d4
Gerald Valverde <gerald-_16@hotmail.com>
2020-11-10 15:41:02 -0600

    read Method

---
+++ Extension/roslyn/readSyntax/readSyntax/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Linq;
+
+namespace readSyntax
+{
+    class Program
+    {
+        /*
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var tree = CSharpSyntaxTree.ParseText(@"
+            public class Sample
+            {
+            public string FooProperty {get; set;}
+            public void FooMethod()
+            {
+            }
+            }");
+
+            var members = tree.GetRoot().DescendantNodes().OfType<MemberDeclarationSyntax>();
+
+            foreach (var member in members)
+            {
+                var property = member as PropertyDeclarationSyntax;
+                if (property != null)
+                    Console.WriteLine("Property: " + property.Identifier);
+                var method = member as MethodDeclarationSyntax;
+                if (method != null)
+                    Console.WriteLine("Method: " + method.Identifier);
+            }
+        }*/
+    }
+}
